b226c7ba3c75184dd1233c54fb83876ec9fac8f8
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2025-03-09 19:05:17 +0600

    Update useWindowSize.js

--- hooks/useWindowSize.js
+++ hooks/useWindowSize.js
@@ -2,19 +2,15 @@ import { useState, useEffect } from "react";
 
 // Hook
 const useWindowSize = () => {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+
     const [windowSize, setWindowSize] = useState({
         width: undefined,
         height: undefined
     });
 
     useEffect(() => {
-        // only execute all the code below in client side
         if (typeof window !== "undefined") {
-            // Handler to call on window resize
             function handleResize() {
-                // Set window width/height to state
                 setWindowSize({
                     width: window.innerWidth,
                     height: window.innerHeight
