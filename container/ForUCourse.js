16c56911ad74317cfce4292d62cdee89ec64a06e
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2025-04-14 14:28:10 +0600

    Update ForUCourse.js

--- container/ForUCourse.js
+++ container/ForUCourse.js
@@ -13,8 +13,6 @@ export default function Courses() {
                         <button className="border-[1px] hover:border-[1px] border-primary px-5 bg-primary hover:bg-white hover:text-primary block text-white py-[6px]  rounded">View More</button>
                     </div>
                 </div>
-                
-
             </div>
         </div>
     );
