16d5920c31c790bc7c0f55ee28a6b45516234691
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2026-02-12 22:33:05 +0600

    Add new team member Md. Wakiullah

--- data/Team.js
+++ data/Team.js
@@ -9,6 +9,7 @@ export const TeamMembers = [
         image: 'https://th-thumbnailer.cdn-si-edu.com/gnVVC1OLQs1cWyFWBciZSwXKon8=/1072x720/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg',
     },
     {
+        
         name: 'Md. Wakiullah',
         title: 'Co-Founder',
         role: 'Ekkhuny Somoy',
