f2937945311ecb111bf3584e7c2579b5904b3aab
Md Wakiullah <74135682+wakiullah@users.noreply.github.com>
2026-02-12 22:32:40 +0600

    Update Team.js

--- data/Team.js
+++ data/Team.js
@@ -22,7 +22,8 @@ export const TeamMembers = [
         title: 'Co-Founder',
         role: 'Ekkhuny Somoy',
         education: 'Department of Arabic',
-        institute: 'University of Chittagong',
+        institute: 'University of Chittagong'
+        ,
         email: 'maria.chen@northsouth.edu',
         image: 'https://th-thumbnailer.cdn-si-edu.com/gnVVC1OLQs1cWyFWBciZSwXKon8=/1072x720/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg',
     },
